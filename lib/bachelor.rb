52004ef2ebb2cdae99c186180a698258945a159e
Oleg Chursin <olegchursin@gmail.com>
2017-12-04 19:55:55 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -9,7 +9,9 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data[season].each do |hsh|
+    if hsh["occupation"].downcase == occupation
+      return hsh["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
