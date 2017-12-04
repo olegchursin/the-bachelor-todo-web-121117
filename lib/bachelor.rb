997c4510af5c1800a80cae5fb05da0ee16665cd5
Oleg Chursin <olegchursin@gmail.com>
2017-12-04 20:04:27 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -19,7 +19,15 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, contestants|
+    contestants.each do |hsh|
+      if hsh["hometown"] == hometown
+       counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
